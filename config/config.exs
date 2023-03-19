0357585b68d01e53098bf15f517c8b134b4fd867
Peter Ullrich <peter.ullrich@remote.com>
2023-03-19 14:32:25 +0100

    Add stores

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,6 @@
 import Config
 
-config :lemon_ex, :api_key, System.get_env("LEMONSQUEEZY_API_KEY")
+config :lemon_ex,
+       :api_key,
+       System.get_env("LEMONSQUEEZY_API_KEY") ||
+         raise("Missing environment variable LEMONSQUEEZY_API_KEY.")
